4bb40d697241a34a0b44dc14446389572676c810
Matheus Cardoso <cardoso.matheusbs@gmail.com>
2025-02-19 12:52:50 -0300

    feat: add home page

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,7 +2,7 @@ import type { ComponentProps } from 'react'
 
 interface ButtonProps extends ComponentProps<'button'> {}
 
-export default function Button(props: ButtonProps) {
+export function Button(props: ButtonProps) {
   return (
     <button
       className="flex justify-between items-center px-5 h-12 w-full bg-gray-500 text-blue font-semibold rounded-xl cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
